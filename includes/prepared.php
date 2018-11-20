63e4c38da6ee339b1bcb8f6c76a8f02d25967e66
hacpaka <hacpaka@eggbe.com>
2018-11-20 23:30:03 +0300

    critical bug fixed

--- includes/prepared.php
+++ includes/prepared.php
@@ -12,7 +12,7 @@ public function h($v){ return is_object($v) && method_exists($v, 'toHtml')
 
 public function f($v){ return array_filter($v, function ($s){
 return !in_array($s, ['GLOBALS', '_POST', '_GET', '_FILES', '_SERVER',
-'_COOKIE', '__export', '__data', '__obj']) ; }, ARRAY_FILTER_USE_KEY); }
+'_COOKIE', '__export', '__data']) ; }, ARRAY_FILTER_USE_KEY); }
 
 public function s($n){ static $s = []; if (func_num_args() < 2) { if (isset($s[$n])) { foreach ($s[$n] as $o) {
 echo $o; }}} else {if (!isset($s[$n])){ $s[$n] = []; } ob_start(); $l = ob_get_level(); call_user_func_array(func_get_arg(1),
