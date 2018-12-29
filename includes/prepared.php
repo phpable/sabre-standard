d60a62239411fb806980612a8e3bb39c5472935a
hacpaka <hacpaka@eggbe.com>
2018-12-29 11:47:57 +0300

    refactoring

--- includes/prepared.php
+++ includes/prepared.php
@@ -1,78 +1,74 @@
 <?php
-if (!function_exists('__init')) {
-	function __init() {
-		return new class {
+return new class {
 
-			/**
-			 * Converts an argument into a string.
-			 *
-			 * @param mixed $value
-			 * @return string
-			 */
-			public function o($value) {
-				return is_object($value) && method_exists($value, '__toString')
-					? $value->__toString() : strval($value);
-			}
-
-			/**
-			 * Converts an argument into a string and escapes HTML entities.
-			 *
-			 * @param mixed $value
-			 * @return string
-			 */
-			public function h($value) {
-				return is_object($value) && method_exists($value, 'toHtml')
-					? $value->toHtml() : htmlentities($this->o($value), ENT_QUOTES, "UTF-8", false);
-			}
-
-			/**
-			 * Filters an array by removing all special-using keys.
-			 *
-			 * @param array $value
-			 * @return array
-			 */
-			public function f($value) {
-				return is_array($value) ? (array_filter($value, function ($key) {
-					return !in_array($key, ['GLOBALS', '_POST', '_GET', '_REQUEST', '_FILES',
-						'_SESSION', '_SERVER', '_ENV', '_COOKIE']);
-				}, ARRAY_FILTER_USE_KEY)) : [];
-			}
-
-			/**
-			 * Collects section content returned by a handler
-			 * or prints previously collected fragments if the second argument isn't specified.
-			 *
-			 * @param string $name
-			 * @param callable handler
-			 * @return void
-			 */
-			public function c($name,  $handler = null): void {
-				static $Cache = [];
-
-				if (is_null($handler)) {
-					if (isset($Cache[$name])) {
-						foreach ($Cache[$name] as $o) {
-							echo $o;
-						}
-
-						unset($Cache[$name]);
-					}
-				} else {
-					ob_start();
-					$level = ob_get_level();
-
-					call_user_func($handler, ...array_slice(func_get_args(), 2));
-					while (ob_get_level() > $level) {
-						ob_get_clean();
-					}
-
-					if (!isset($Cache[$name])) {
-						$Cache[$name] = [];
-					}
-
-					array_push($Cache[$name], ob_get_clean());
-				}
-			}
-		};
+	/**
+	 * Converts an argument into a string.
+	 *
+	 * @param mixed $value
+	 * @return string
+	 */
+	public function o($value) {
+		return is_object($value) && method_exists($value, '__toString')
+			? $value->__toString() : strval($value);
 	}
-}
+
+	/**
+	 * Converts an argument into a string and escapes HTML entities.
+	 *
+	 * @param mixed $value
+	 * @return string
+	 */
+	public function h($value) {
+		return is_object($value) && method_exists($value, 'toHtml')
+			? $value->toHtml() : htmlentities($this->o($value), ENT_QUOTES, "UTF-8", false);
+	}
+
+	/**
+	 * Filters an array by removing all special-using keys.
+	 *
+	 * @param array $value
+	 * @return array
+	 */
+	public function f($value) {
+		return is_array($value) ? (array_filter($value, function ($key) {
+			return !in_array($key, ['GLOBALS', '_POST', '_GET', '_REQUEST', '_FILES',
+				'_SESSION', '_SERVER', '_ENV', '_COOKIE']);
+		}, ARRAY_FILTER_USE_KEY)) : [];
+	}
+
+	/**
+	 * Collects section content returned by a handler
+	 * or prints previously collected fragments if the second argument isn't specified.
+	 *
+	 * @param string $name
+	 * @param callable handler
+	 * @return void
+	 */
+	public function c($name,  $handler = null): void {
+		static $Cache = [];
+
+		if (is_null($handler)) {
+			if (isset($Cache[$name])) {
+				foreach ($Cache[$name] as $o) {
+					echo $o;
+				}
+
+				unset($Cache[$name]);
+			}
+		} else {
+			ob_start();
+			$level = ob_get_level();
+
+			call_user_func($handler, ...array_slice(func_get_args(), 2));
+			while (ob_get_level() > $level) {
+				ob_get_clean();
+			}
+
+			if (!isset($Cache[$name])) {
+				$Cache[$name] = [];
+			}
+
+			array_push($Cache[$name], ob_get_clean());
+		}
+	}
+};
--- src/Delegate.php
+++ src/Delegate.php
@@ -89,35 +89,19 @@ class Delegate extends AFacade {
 	}
 
 	/**
-	 * @var string[]
-	 */
-	private static $History = [];
-
-	/**
 	 * @return string[]
+	 * @throws \Throwable
 	 */
-	public final static function history(){
-		return self::$History;
+	public final static function history(): array {
+		return self::getRecipientInstance()->history();
 	}
-
-	/**
-	 * @const int
-	 */
-	public const CO_SKIP_RAW = 0b0001;
-
-	/**
-	 * @const int
-	 */
-	public const CO_SKIP_CALL = 0b0010;
 
 	/**
 	 * Initialize the standard compiler's behavior.
 	 * @throws \Exception
 	 */
 	protected final static function initialize(): SInit {
-		return new SInit([function($filepath){
-			array_push(self::$History, $filepath);
-		}], function(){
+		return new SInit([], function(){
 			try {
 				if (!file_exists($Path = (new Path(__DIR__))->getParent()->append('includes', 'behavior.php'))) {
 					throw new \Exception('Can not load behavior!');
@@ -135,31 +119,20 @@ class Delegate extends AFacade {
 
 	/**
 	 * @param IReader $Reader
-	 * @param int $mode
-	 * @param string $name
 	 * @return \Generator
 	 * @throws \Exception
 	 */
-	public static final function compile(IReader $Reader,  string $name = null, int $mode = 0b0000): \Generator {
-		self::$History = [];
-
-		$name = $name ?? 'main_' . md5(Str::join('|', microtime(true),
-			__CLASS__, __METHOD__, $Reader->getLocation()));
-
-		yield '<?php if (!function_exists("' . $name
-			. '")){ function ' . $name . '($__obj, $__data){ extract($__data); unset($__data); ?>';
+	public static final function compile(IReader $Reader): \Generator {
+		yield '<?php call_user_func(function($__obj, $__data){'
+			. 'extract($__data); unset($__data); ?>';
 
 		yield from parent::compile($Reader);
 
-		yield '<?php }}?>';
+		yield '<?php }, call_user_func(function(){ ?>';
 
-		if (~$mode & self::CO_SKIP_RAW) {
-			yield (new Php())->minify(Path::create(dirname(__DIR__),
-				'includes', 'prepared.php')->toFile()->getContent() . ' ?>');
-		}
+		yield (new Php())->minify(Path::create(dirname(__DIR__),
+			'includes', 'prepared.php')->toFile()->getContent() . ' ?>');
 
-		if (~$mode & self::CO_SKIP_CALL) {
-			yield '<?php ' . $name . '(__init(), $__data ?? []);?>';
-		}
+		yield '<?php }), $__data);?>';
 	}
 }
